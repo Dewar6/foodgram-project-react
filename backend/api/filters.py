71a66f53b73b467b0c3044a41c8886e35219d28f
minin_dm <minin_dm@mail.ru>
2023-07-11 14:47:11 +0300

    допилил фильтрацию

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -18,9 +18,9 @@ class RecipesFilter(FilterSet):
         to_field_name='slug',
         queryset=Tag.objects.all()
     )
-    is_favorited = filters.BooleanFilter(field_name='is_favorited')
+    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
-        field_name='is_in_shopping_cart')
+        method='filter_is_in_shopping_cart')
 
     class Meta:
         model = Recipe
@@ -30,3 +30,13 @@ class RecipesFilter(FilterSet):
             'is_favorited',
             'is_in_shopping_cart',
         )
+
+    def filter_is_favorited(self, queryset, name, value):
+        if value and self.request.user.is_authenticated:
+            return queryset.filter(favoriterecipe__user=self.request.user)
+        return queryset
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        if value and self.request.user.is_authenticated:
+            return queryset.filter(shoppingcart__user=self.request.user)
+        return queryset
