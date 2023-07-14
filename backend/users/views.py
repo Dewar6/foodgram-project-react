7acc62cc0e40b4fe0d17d9084bd38f0fccf60fed
minin_dm <minin_dm@mail.ru>
2023-07-14 15:06:22 +0300

    отправка на третье ревью

--- backend/users/views.py
+++ backend/users/views.py
@@ -6,7 +6,6 @@ from rest_framework.response import Response
 
 from api.permissions import CreateAnyOtherAuthenticatedPermission
 from api.serializers import SubscribeSerializer
-from recipes.models import Recipe
 from users.models import User, UserSubscribe
 from users.pagination import CustomPagination
 from users.serializers import CustomUserSerializer
